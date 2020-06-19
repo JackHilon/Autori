6e98ae71995319cde4837e910481fc883e3abe0d
markandmark1111@gmail.com <markandmark1111@gmail.com>
2020-06-19 15:51:15 +0200

    Add project files.

---
+++ Autori/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Autori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Autori
+            // https://open.kattis.com/problems/autori
+
+
+            var names = Enterline();
+            string reslut = "";
+            for (int i = 0; i < names.Length; i++)
+            {
+                reslut = reslut + FirstChar(names[i]);
+            }
+            Console.WriteLine(reslut);
+        }
+
+        private static string FirstChar(string str)
+        {
+            return str[0].ToString();
+        }
+
+        private static string[] Enterline()
+        {
+            return Console.ReadLine().Split('-');
+        }
+    }
+}
